54c9efd7cedaf4c6b599456cd225ec88370629dd
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-12-10 23:34:30 -0300

    Conectando com MongoDB repository e service

--- src/main/java/com/victor/workshopmongo/domain/User.java
+++ src/main/java/com/victor/workshopmongo/domain/User.java
@@ -3,10 +3,17 @@ package com.victor.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+//para informar que a class corresponde a uma coleção do mongoDB
+@Document(collection = "user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
@@ -14,7 +21,6 @@ public class User implements Serializable {
 	public User() {}
 
 	public User(String id, String name, String email) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
