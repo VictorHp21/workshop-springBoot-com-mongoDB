20dc2da749e2b40fd5cb7c70be9c00b6e4adc947
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-12-13 21:15:54 -0300

    Consulta vários critérios

--- src/main/java/com/victor/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/victor/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.victor.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.victor.workshopmongo.domain.Post;
@@ -12,5 +14,13 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	// querry method
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findBySearchTitle(String text);
+	
+	// consulta com vários critérios
+	@Query("{ $and: [ { date: { $gte: ?1 } }\r\n"
+			+ ", { date: { $lte: ?2 } }, ... , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, ... , { 'comments.text': { $regex: ?0, $options: 'i' } } ] }] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 	
 }
